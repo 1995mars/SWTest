50e0133295616c8e3ee9bb750756766189427a76
Mars <51732119+1995mars@users.noreply.github.com>
2025-08-01 15:37:25 +0700

    Create Test.java

---
+++ src/main/java/org/mars/java/Test.java
@@ -0,0 +1,12 @@
+package org.mars.java;
+
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4};
+        int b[] = {1,2,4};
+        System.out.println(Arrays.compare(a, b));
+    }
+}
